cff08018ccd556768293a938c550652b22bee0ed
0sakib0 <88287970+0sakib0@users.noreply.github.com>
2023-02-05 00:59:19 +0600

    Every button functional but slider and layout is still ugly

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 console.log("mango");
 
 const grid = document.querySelector(".grid-container");
+const eraseButton = document.querySelector(".color-button")
+const clearButton = document.querySelector(".clear-button");
+const blackButton = document.querySelector(".color-button");
+const sizeSlider = document.getElementById("size-slider");
+const sizeValue = document.getElementById("size-value");
 
 
+
+sizeValue.textContent = sizeSlider.value;
+createGrid(sizeSlider.value);
+
+sizeSlider.addEventListener("input", (e) => {
+  sizeValue.textContent = e.target.value;
+  grid.innerHTML = '';
+  createGrid(e.target.value);
+});
+// board cell creator function //
 function createGrid(size) {
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-  
+  grid.innerHTML = "";
   for (let i = 0; i < size ** 2; i++) {
     const div = document.createElement("div");
     div.classList.add('grid-cell')
@@ -16,19 +31,42 @@ function createGrid(size) {
 }
 
 
-grid.addEventListener("pointermove", (e) => {
-  if (e.target.nodeName === "DIV") {
-    e.target.style.backgroundColor = "black";
-  }
+// color and erase button functionality //
+const colorButtons = document.querySelectorAll(".color-button");
+let color = "";
+
+colorButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    color = e.target.dataset.color;
+  });
 });
 
-const clearButton = document.querySelector(".clear-button");
+grid.addEventListener("mousedown", (e) => {
+  grid.addEventListener("mouseover", paintCell);
+});
 
+grid.addEventListener("mouseup", () => {
+  grid.removeEventListener("mouseover", paintCell);
+});
+
+function paintCell(e) {
+  if (e.target.nodeName === "DIV") {
+    e.target.style.backgroundColor = color;
+  }
+}
+
+// clear button function //
 clearButton.addEventListener("click", () => {
+  isDrawing = false;
   const cells = document.querySelectorAll(".grid-cell");
   cells.forEach((cells) => {
     cells.style.backgroundColor = "white";
   });
 });
 
-createGrid(32);
+// functions to run //
+createGrid(16);
+blackColor();
+
+
+
